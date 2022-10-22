ab107dbd17aa181c7f760cf5bd6c086e3bb1c138
Jim Drury (he/him) <708229+geometricpanda@users.noreply.github.com>
2022-10-22 10:25:52 +0100

    Create terraform.yml

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "3.5.0"
     }
   }
 }
 
 provider "google" {
-  credentials = file(".credentials/geometricpanda-4824ec9a6a50.json")
+  credentials = file(".credentials/credential.json")
 
   project = "geometricpanda"
   region  = "europe-west2"
